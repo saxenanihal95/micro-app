8c3694bbcfaf9e5cf9ffc458eb005bb04337a4af
nihal.s <nihal.s@simformsolutions.com>
2021-05-05 18:07:32 +0530

    Added process.env

--- packages/core-ui/build-utils/webpack.prod.js
+++ packages/core-ui/build-utils/webpack.prod.js
@@ -1,4 +1,13 @@
+const { DefinePlugin } = require('webpack');
+ 
 module.exports = {
   mode: 'production',
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      }
+    }),
+  ],
   devtool: 'source-map',
 };
